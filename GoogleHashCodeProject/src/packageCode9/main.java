2cc6f813e6bb639bd1be88561a4087915e041411
skuduzovic <skuduzovic@echots.com>
2018-03-01 18:07:26 +0100

    Scanner

--- GoogleHashCodeProject/src/packageCode9/main.java
+++ GoogleHashCodeProject/src/packageCode9/main.java
@@ -2,10 +2,11 @@ package packageCode9;
 
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.util.Scanner;
 
 class main {
 
 	public static void main(String[] args) {
-
+		
 	}
 }
